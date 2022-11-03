e10da92f4c9692bc24144c92b9d0aa87514bc747
Trygve Amundsen <trygveamundsen@live.no>
2022-11-03 21:52:49 +0100

    cleaned up

--- LSTM/main.py
+++ LSTM/main.py
@@ -1,3 +1,9 @@
+
+from numpy import array
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Bidirectional
 import numpy as np
 import time as tm
 import datetime as dt
@@ -6,107 +12,112 @@ from yahoo_fin import stock_info as yf
 from sklearn.preprocessing import MinMaxScaler
 from collections import deque
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
+from keras.layers import Dense, LSTM, Dropout, Bidirectional
 import matplotlib.pyplot as plt
 
 
-N_STEPS = 7
-
-
 class Lstm:
-    def __init__(self,stock,data):
-        self.stock =  stock
-        self.df = data
 
-    def Model(x_train, y_train):
+    def __init__(self):
+        print('started')
+
+    def Model(self,n_steps,n_features):
         model = Sequential()
-        model.add(LSTM(60, return_sequences=True, input_shape=(N_STEPS, len(['close']))))
-        model.add(Dropout(0.5))
-        model.add(LSTM(120, return_sequences=False))
-        model.add(Dropout(0.5))
-        model.add(Dense(20))
+        model.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(n_steps, n_features)))
+        model.add(LSTM(50, activation='relu'))
         model.add(Dense(1))
-        BATCH_SIZE = 8
-        EPOCHS = 100
-        model.compile(loss='mean_squared_error', optimizer='adam')
-        model.fit(x_train, y_train,
-                    batch_size=BATCH_SIZE,
-                    epochs=EPOCHS,
-                    verbose=1)
-        model.summary()
+        model.compile(optimizer='adam', loss='mse',metrics=['mae'])
         return model
 
-    def Train(self,days):
-        df = df.copy()
-        df['future'] = df['close'].shift(-days)
-        last_sequence = np.array(df[['close']].tail(days))
-        df.dropna(inplace=True)
-        sequence_data = []
-        sequences = deque(maxlen=N_STEPS)
+    def SplitSequence(self,sequence, n_steps):
+        X, y = list(), list()
+        for i in range(len(sequence)):
+            end_ix = i + n_steps
+            if end_ix > len(sequence)-1:
+                break
+            seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+            X.append(seq_x)
+            y.append(seq_y)
+        return array(X), array(y)
 
-        for entry, target in zip(df[['close'] + ['date']].values, df['future'].values):
-            sequences.append(entry)
-            if len(sequences) == N_STEPS:
-                sequence_data.append([np.array(sequences), target])
+def getTimeInterval():
+    date_now = tm.strftime('%Y-%m-%d')
+    date_3_years_back = ((dt.date.today() - dt.timedelta(days=1)) - dt.timedelta(days=50)).strftime('%Y-%m-%d')
+    return date_now, date_3_years_back
 
-        last_sequence = list([s[:len(['close'])] for s in sequences]) + list(last_sequence)
-        last_sequence = np.array(last_sequence).astype(np.float32)
+def scaleData(data):
+    scaler = MinMaxScaler()
+    data['close'] = scaler.fit_transform(np.expand_dims(data['close'].values, axis=1))
+    return data
 
-        # construct the X's and Y's
-        X, Y = [], []
-        for seq, target in sequence_data:
-            X.append(seq)
-            Y.append(target)
-
-        # convert to numpy arrays
-        X = np.array(X)
-        Y = np.array(Y)
-
-        return df, last_sequence, X, Y
-
-
-STOCK = "AAPL"
-
-date_now = tm.strftime('%Y-%m-%d')
-date_3_years_back = ((dt.date.today() - dt.timedelta(days=1)) - dt.timedelta(days=1104)).strftime('%Y-%m-%d')
-
-df = yf.get_data(
-    STOCK, 
+def getCleanData(stock):
+    date_now,date_3_years_back = getTimeInterval()
+    init_df = yf.get_data(
+    stock, 
     start_date=date_3_years_back, 
-    end_date=date_now, 
+    end_date=date_now,
+
     interval='1d')
-
-df = df.drop(['open', 'high', 'low', 'adjclose', 'ticker', 'volume'], axis=1)
-df['date'] = df.index
-
-plt.style.use(style='ggplot')
-plt.figure(figsize=(16,10))
-plt.plot(df['close'][-200:])
-plt.xlabel("days")
-plt.ylabel("price")
-plt.legend([f'Actual price for {STOCK}'])
-plt.show()
-
-scaler = MinMaxScaler()
-df['close'] = scaler.fit_transform(np.expand_dims(df['close'].values, axis=1))
-
-lstm = Lstm("AAPL",df)
+    init_df = init_df.drop(['open', 'high', 'low', 'adjclose', 'ticker', 'volume'], axis=1)
+    init_df['date'] = init_df.index
+    return init_df
 
 
-predictions = []
-days = [1]
 
-for step in days:
-  df, last_sequence, x_train, y_train = lstm.Train(step)
-  x_train = x_train[:, :, :len(['close'])].astype(np.float32)
 
-  model = lstm.Model(x_train,y_train)
+def main():
+    stock = 'KAHOT.OL'
+    n_features = 1
+    n_steps = 3
 
-  last_sequence = last_sequence[-N_STEPS:]
-  last_sequence = np.expand_dims(last_sequence, axis=0)
-  prediction = model.predict(last_sequence)
-  predicted_price = scaler.inverse_transform(prediction)[0][0]
+    lstm = Lstm()
+    scaler = MinMaxScaler()
 
-  predictions.append(round(float(predicted_price), 2))
 
-  print(predictions)
+    data = getCleanData(stock)
+    data['close'] = scaler.fit_transform(np.expand_dims(data['close'].values, axis=1))
+
+
+
+    raw_seq = data['close']
+    X, y = lstm.SplitSequence(raw_seq,n_steps)
+
+    X = X.reshape((X.shape[0], X.shape[1], n_features))
+    
+    model = lstm.Model(n_steps,n_features)
+    history  = model.fit(X, y, epochs=100, verbose=1)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(history.history['mae'], label='mae')
+    plt.plot(history.history['loss'], label='loss')
+    plt.legend()
+    plt.show()
+
+    x_input = array(data['close'][-3:])
+    x_input = x_input.reshape((1, n_steps, n_features))
+    yhat = model.predict(x_input, verbose=1)
+    print(yhat)
+
+    scaled_yhat = scaler.inverse_transform(yhat)[0][0]
+    print(scaled_yhat)
+
+
+    p = array(data['close'])
+    p = p.reshape((1,len(p),1))
+
+    forecast = model.predict(p)
+    print(forecast)
+    actual = data['close']
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(forecast, label="predicted")
+    plt.plot(actual, label="actual")
+    plt.xlabel("Timestep")
+    plt.ylabel("Value")
+    plt.legend()
+    plt.show()
+
+
+
+
+main()
