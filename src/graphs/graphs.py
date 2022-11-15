d540e2c413742a5273b83c6976e8b0f39c751c14
Hanna Nordhus <hannagn@stud.ntnu.no>
2022-11-15 12:05:26 +0100

    Refactored make_graph method

--- src/graphs/graphs.py
+++ src/graphs/graphs.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 #predictions = [[50,25,35],[150,150,150],[100,100,100]]
 
-def make_graph(data,predictions):
+def make_graph(data,predictions, stock):
     methods = len(predictions)
     days = len(predictions[0])
     plt.style.use(style='ggplot')
@@ -18,6 +18,6 @@ def make_graph(data,predictions):
         plt.plot(temp['close'].tail(4))
     plt.xlabel('days')
     plt.ylabel('price')
-    plt.legend([f'Actual price for {"halo"}', 
+    plt.legend([f'Actual price for {stock}', 
                 f'Predicted price for future 3 days'])
     plt.show()
